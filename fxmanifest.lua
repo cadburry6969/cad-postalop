23c1b2def081e2d51cf943ce00fc852a709cbec2
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-05-06 16:21:05 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@ game "gta5"
 
 description "Postal Op Job which uses qb-target & polyzone"
 author "Cadburry#7547"
+version "1.0.0"
 
 server_script 'sv_postalop.lua'
 client_script 'cl_postalop.lua'
